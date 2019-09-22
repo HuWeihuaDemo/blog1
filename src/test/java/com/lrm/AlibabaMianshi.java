bdd8f1f4114129575fa3063019b76decdf6114b2
HuWeihuaDemo <rufengweiweiji@163.com>
2019-09-22 19:59:50 +0800

    Initial commit

---
+++ src/test/java/com/lrm/AlibabaMianshi.java
@@ -0,0 +1,24 @@
+package com.lrm;
+
+public class AlibabaMianshi {
+    //阿里巴巴经典面试题
+/**    public static void main(String[] args) {
+        Integer a = new Integer(5);
+        Integer b = new Integer(5);
+
+        Integer c = 5;
+        Integer d = 5;
+
+        Integer e = 128;
+        Integer f = 128;
+
+        int g = 128;
+
+        System.out.println(a == b);
+        System.out.println(b == c);
+        System.out.println(c == d);
+        System.out.println(e == f);
+        System.out.println(f == g);
+    }
+ */
+}
